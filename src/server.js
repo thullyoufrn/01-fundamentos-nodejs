90cbd512d77dccb18af61feb21810f34e2429e8b
Thullyo Damasceno <thullyo.damasceno@logiquesistemas.com.br>
2023-12-07 14:41:16 -0300

    Insomnia setup and middleware

--- src/server.js
+++ src/server.js
@@ -1,23 +1,28 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  if (method === 'GET' && url === '/users' ) {
+  await json(req, res)
+
+  if (method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
-  if (method === 'POST' && url === '/users' ) {
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'Thullyo Damasceno',
-      email: 'thullyodamasceno@example.com'
+      name,
+      email,
     })
-    
+  
     return res.writeHead(201).end()
   }
 
